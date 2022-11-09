d125eb4e73a45a2ca30a86ab3cfcacf524a3ef5e
Tobias Lindström <tobias.lindstrom@interactivesolutions.se>
2022-11-09 16:51:58 +0100

    Upgrade packages

--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -1,24 +1,6 @@
 import 'styled-components';
-import { ThemeDefault, ThemePaletteRange } from '@entire.se/components';
+import { ThemeDefault } from '@entire.se/components';
 
 declare module 'styled-components' {
-  export interface DefaultTheme extends ThemeDefault {
-    palettes: ThemeDefault['palettes'] & {
-      [name in 'primary' | 'error' | 'success' | 'info' | 'warning']: {
-        [range in ThemePaletteRange]: string;
-      };
-    };
-    surfaces: ThemeDefault['surfaces'] & {
-      primary: string;
-      onPrimary: string;
-      error: string;
-      onError: string;
-      success: string;
-      onSuccess: string;
-      info: string;
-      onInfo: string;
-      warning: string;
-      onWarning: string;
-    };
-  }
+  export interface DefaultTheme extends ThemeDefault {}
 }
