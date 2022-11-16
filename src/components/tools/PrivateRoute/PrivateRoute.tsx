793fffe259de17d9b51b82d66e0086ae48ebc66b
Tobias Lindström <tobias.lindstrom@interactivesolutions.se>
2022-11-16 11:46:50 +0100

    Private route

---
+++ src/components/tools/PrivateRoute/PrivateRoute.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from 'react';
+
+import { useRouter } from 'next/router';
+
+import { Paths } from 'consts/router';
+import { useAuthenticationStore } from 'store/authentication';
+
+export interface PrivateRouteProps {
+  children: ReactNode;
+}
+
+export const PrivateRoute = ({ children }: PrivateRouteProps) => {
+  const router = useRouter();
+  const user = useAuthenticationStore((store) => store.user);
+
+  if (!user) {
+    router.replace(Paths.Home);
+  }
+
+  // eslint-disable-next-line react/jsx-no-useless-fragment
+  return <>{children}</>;
+};
