3c6c9365c6b16280a57ed48293cf39ed6a54ab6d
Tobias Lindström <tobias.lindstrom@interactivesolutions.se>
2023-02-28 17:05:25 +0100

    MUI

--- src/routes/Login/Login.tsx
+++ src/routes/Login/Login.tsx
@@ -1,4 +1,5 @@
-import { Typography } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+import { Button, ButtonGroup, Typography } from '@mui/material';
 import { useIntl } from 'react-intl';
 
 import { SEO } from 'components/tools';
@@ -14,6 +15,28 @@ export const Login = () => {
       <Typography variant="h4" color="info.dark">
         {'Login'.toString()}
       </Typography>
+      <div>
+        <Button size="small" variant="contained">
+          {'small'.toString()}
+        </Button>
+        <Button size="medium" variant="contained">
+          {'medium'.toString()}
+        </Button>
+        <LoadingButton size="large" variant="contained" loading>
+          {'large'.toString()}
+        </LoadingButton>
+      </div>
+      <ButtonGroup>
+        <LoadingButton size="small" variant="contained" loading>
+          {'small'.toString()}
+        </LoadingButton>
+        <Button size="medium" variant="contained">
+          {'medium'.toString()}
+        </Button>
+        <Button size="large" variant="contained">
+          {'large'.toString()}
+        </Button>
+      </ButtonGroup>
     </>
   );
 };
--- src/styles/theme/components.tsx
+++ src/styles/theme/components.tsx
@@ -8,6 +8,30 @@ export const components: ThemeOptions['components'] = {
       component: NextLinkStyled
     } as LinkProps
   },
+  MuiButton: {
+    styleOverrides: {
+      root: () => ({
+        textTransform: 'none'
+      }),
+      sizeSmall: ({ theme }) => ({
+        padding: theme.spacing(0.5, 1),
+        borderRadius: 15
+      }),
+      sizeMedium: ({ theme }) => ({
+        padding: theme.spacing(1, 2),
+        borderRadius: 20
+      }),
+      sizeLarge: ({ theme }) => ({
+        padding: theme.spacing(1.5, 3),
+        borderRadius: 25
+      })
+    },
+    defaultProps: {
+      disableRipple: true,
+      disableElevation: true,
+      LinkComponent: NextLinkStyled
+    }
+  },
   MuiButtonBase: {
     defaultProps: {
       disableRipple: true,
@@ -16,7 +40,8 @@ export const components: ThemeOptions['components'] = {
   },
   MuiButtonGroup: {
     defaultProps: {
-      disableRipple: true
+      disableRipple: true,
+      disableElevation: true
     }
   }
 };
