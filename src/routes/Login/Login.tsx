c494018b5c1b3b0c7b2888c481bb01743a57dbaa
Tobias Lindström <tobias.lindstrom@interactivesolutions.se>
2023-03-09 17:44:29 +0100

    MUI

--- src/routes/Login/Login.tsx
+++ src/routes/Login/Login.tsx
@@ -1,13 +1,43 @@
+import { useCallback, useMemo } from 'react';
+
 import { Typography } from '@mui/material';
 import { useIntl } from 'react-intl';
 
 import { SEO } from 'components/tools';
 import { Table } from 'components/ui/table';
+import { useBooksLazyQuery } from 'graphql/cli/graphql.everything';
+import { TableFetchDataVariables } from 'types/table';
 
 import { texts } from './Login.text';
 
 export const Login = () => {
   const { formatMessage } = useIntl();
+  const [fetchBooks, { data, loading, error }] = useBooksLazyQuery();
+
+  const columns = useMemo(
+    () => [
+      {
+        header: 'Title',
+        accessorKey: 'title'
+      }
+    ],
+    []
+  );
+
+  const handleFetchData = useCallback(
+    (variables: TableFetchDataVariables) => {
+      console.log(variables);
+      fetchBooks({
+        variables: {
+          filter: {
+            offset: variables.pageIndex * variables.pageSize,
+            limit: variables.pageSize
+          }
+        }
+      });
+    },
+    [fetchBooks]
+  );
 
   return (
     <>
@@ -15,7 +45,14 @@ export const Login = () => {
       <Typography variant="h4" color="info.dark">
         {'Login'.toString()}
       </Typography>
-      <Table />
+      <Table
+        columns={columns}
+        data={data?.books.edges || []}
+        onFetchData={handleFetchData}
+        pagination
+        loading={loading}
+        error={error}
+      />
     </>
   );
 };
