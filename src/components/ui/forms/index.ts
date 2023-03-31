cf8d88574953c436aa0cd80766701d0e858bbfd0
Tobias Lindström <tobias.lindstrom@interactivesolutions.se>
2023-03-31 14:53:13 +0200

    Export components

--- src/components/ui/forms/index.ts
+++ src/components/ui/forms/index.ts
@@ -2,3 +2,6 @@ export * from './TextField/TextField';
 export * from './FormError/FormError';
 export * from './Select/Select';
 export * from './Checkbox/Checkbox';
+export * from './Radio/Radio';
+export * from './Switch/Switch';
+export * from './DatePicker/DatePicker';
