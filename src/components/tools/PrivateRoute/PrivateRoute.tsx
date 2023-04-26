9c487b1320b3785cc6f3dd702eab8ff83c53bc50
Daniel Westerling <daniel.westerling@interactivesolutions.se>
2023-04-26 19:51:58 +0200

    Linting fixes

--- src/components/tools/PrivateRoute/PrivateRoute.tsx
+++ src/components/tools/PrivateRoute/PrivateRoute.tsx
@@ -1,8 +1,4 @@
-import { ReactNode, useEffect } from 'react';
-
-import { useRouter } from 'next/router';
-
-import { Paths } from 'consts/router';
+import { ReactNode } from 'react';
 import { useAuthenticationStore } from 'store/authentication';
 
 export type PrivateRouteProps = {
@@ -10,7 +6,6 @@ export type PrivateRouteProps = {
 };
 
 export const PrivateRoute = ({ children }: PrivateRouteProps) => {
-  const router = useRouter();
   const user = useAuthenticationStore((store) => store.user);
 
   // Make sure nothing is visible if not signed in
