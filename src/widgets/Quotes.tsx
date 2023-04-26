9ac66b9dc3ae2a07f66decb9fd4adacb8121d618
Daniel Westerling <daniel.westerling@interactivesolutions.se>
2023-04-26 20:58:15 +0200

    Adding Quotes widget

---
+++ src/widgets/Quotes.tsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+
+export const Quotes = () => {
+  const [joke, setJoke] = useState('');
+
+  useEffect(() => {
+    const fetchData = () => {
+      fetch('https://api.chucknorris.io/jokes/random')
+        .then((response) => response.json())
+        .then((data) => setJoke(data.value))
+        .catch((error) => console.error(error));
+    };
+
+    fetchData();
+
+    const intervalId = setInterval(() => {
+      fetchData();
+    }, 120000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+
+  return <span>{joke}</span>;
+};
