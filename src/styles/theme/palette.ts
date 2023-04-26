45993868f0a01b9331e4b72c2c3a14dfbf0ce721
Tony <tony.karlsson@interactivesolutions.se>
2023-04-26 18:21:12 +0200

    palette

--- src/styles/theme/palette.ts
+++ src/styles/theme/palette.ts
@@ -3,7 +3,19 @@ import { red } from '@mui/material/colors';
 
 export const palette: ThemeOptions['palette'] = {
   primary: {
-    main: '#fe5b3c',
+    main: '#FD5A3B',
+  },
+  secondary: {
+    main: '#390115',
+  },
+  grey: {
+    100: '#ffffff',
+    200: '#FAFAFA',
+    300: '#F2F2F2',
+    400: '#B2B2B2',
+    500: '#262626',
+    600: '#171717',
+    700: '#000000',
   },
   error: {
     main: red.A400,
