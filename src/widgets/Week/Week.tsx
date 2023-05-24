2d1be65c566de53ad2f0b89922b2cfc55cce5705
Robin Andersson <robin.andersson@entire.se>
2023-05-24 21:03:34 +0200

    Week widget, local time, import from react thing

---
+++ src/widgets/Week/Week.tsx
@@ -0,0 +1,33 @@
+import { getWeek } from 'date-fns';
+import { sv } from 'date-fns/locale';
+
+export const Week = () => {
+
+    const currentDate = new Date();
+    const currentWeekNumber = getWeek(currentDate, { locale: sv });
+
+    const headingStyle : React.CSSProperties= {
+        margin: "0",
+        position: "absolute",
+        top: "10px",
+        left: "0",
+        width: "100%",
+    }
+    const containerStyle : React.CSSProperties= {
+        textAlign: "center",
+    }
+    const weekNrStyle = {
+        fontSize: "4rem",
+        fontWeight: "bold",
+        textDcoration: "underline",
+        marginTop: "30px",
+    }
+
+    return (
+        <div style={containerStyle}>
+            <h3 style={headingStyle}>Current week</h3>
+            <div style={weekNrStyle}>{currentWeekNumber}</div>
+        </div>
+    );
+
+};
