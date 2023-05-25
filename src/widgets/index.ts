a4fed83c150f3e35dc30f8bfc2472e994a825dca
Tobias Herlitz <tobias.herlitz@entire.se>
2023-05-25 08:08:46 +0200

    removed placeholder widgets

--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -1,8 +1,6 @@
 import { GridItem } from 'types/grid';
 
 import { Logo } from './Logo';
-import { LoremIpsum } from './LoremIpsum';
-import { LunchToday } from './LunchToday/LunchToday';
 import { Memes } from './Memes';
 import { Quotes } from './Quotes';
 import { That70sClock } from './That70sClock';
@@ -12,14 +10,6 @@ export const gridItems: GridItem[] = [
   {
     Widget: Logo,
     dataGrid: { w: 4, h: 3 },
-  },
-  {
-    Widget: LoremIpsum,
-    dataGrid: { w: 3, h: 3 },
-  },
-  {
-    Widget: LunchToday,
-    dataGrid: { w: 3, h: 3 },
   },
   {
     Widget: Quotes,
