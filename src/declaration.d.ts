6c43bac603ed93490304eeedd0f12280a91994bc
Tobias Herlitz <tobias.herlitz@entire.se>
2023-09-05 16:02:37 +0200

    added import to type declaration file

--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -1,6 +1,35 @@
+import '@mui/material/styles/createPalette';
+
 declare module '@mui/material/styles/createPalette' {
   // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
   interface Palette {
+    brandColors: {
+      burgundy: string;
+      entireOrange: string;
+      lightOrange: string;
+      black: string;
+      darkGray: string;
+      deepPurple: string;
+      lightPurple: string;
+      gray: string;
+      white: string;
+    };
+    textColors: {
+      primary: string;
+      secondary: string;
+    };
+    uiColors: {
+      successGreen: string;
+      errorRed: string;
+      deepPurpleHover: string;
+      entireOrangeHover: string;
+      lightBlack: string;
+      darkWhite: string;
+    };
+  }
+
+  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+  interface PaletteOptions {
     brandColors: {
       burgundy: string;
       entireOrange: string;
