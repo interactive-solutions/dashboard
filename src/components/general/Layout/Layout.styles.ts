666e93a90224ff0b6b0eb3d8727bac58b038d81c
Ulrika Höök <ulrika.hook@interactivesolutions.se>
2023-09-06 15:25:59 +0200

    Implement design in guide page

--- src/components/general/Layout/Layout.styles.ts
+++ src/components/general/Layout/Layout.styles.ts
@@ -1,9 +1,13 @@
 import { styled } from '@mui/material';
 
+import { theme } from 'styles';
+
 export const Root = styled('div')`
   display: flex;
   flex-direction: column;
-  min-height: 100vh;
+  width: 100vw;
+  height: 100vh;
+  padding: ${theme.spacing(12)};
 `;
 
 export const Content = styled('div')`
